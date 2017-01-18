a0a64a59762fa8554eb46b6ec544f70a5d46f551
周晓东 <zhouxiaodong@yaomaitong.cn>
2017-01-18 13:40:29 +0800

    fix 鲁班压缩出现路径重复

--- takephoto_library/src/main/java/com/jph/takephoto/model/LubanOptions.java
+++ takephoto_library/src/main/java/com/jph/takephoto/model/LubanOptions.java
@@ -2,8 +2,6 @@ package com.jph.takephoto.model;
 
 import java.io.Serializable;
 
-import me.shaohui.advancedluban.Luban;
-
 /**
  * Luban配置类
  * Author: crazycodeboy
@@ -13,76 +11,69 @@ import me.shaohui.advancedluban.Luban;
  * GitHub:https://github.com/crazycodeboy
  * Eamil:crazycodeboy@gmail.com
  */
-public class LubanOptions implements Serializable{
-    /**
-     * 压缩到的最大大小，单位B
-     */
-    private int maxSize;
-    private int maxHeight;
-    private int maxWidth;
-    private int gear= Luban.CUSTOM_GEAR;
-    private LubanOptions(){}
+public class LubanOptions implements Serializable {
+  /**
+   * 压缩到的最大大小，单位B
+   */
+  private int maxSize;
+  private int maxHeight;
+  private int maxWidth;
 
-    public int getMaxSize() {
-        return maxSize;
+  private LubanOptions() {
+  }
+
+  public int getMaxSize() {
+    return maxSize;
+  }
+
+  public void setMaxSize(int maxSize) {
+    this.maxSize = maxSize;
+  }
+
+  public int getMaxHeight() {
+    return maxHeight;
+  }
+
+  public void setMaxHeight(int maxHeight) {
+    this.maxHeight = maxHeight;
+  }
+
+  public int getMaxWidth() {
+    return maxWidth;
+  }
+
+  public void setMaxWidth(int maxWidth) {
+    this.maxWidth = maxWidth;
+  }
+
+  public static class Builder {
+    private LubanOptions options;
+
+    public Builder() {
+      options = new LubanOptions();
     }
 
-    public void setMaxSize(int maxSize) {
-        this.maxSize = maxSize;
+    public Builder setMaxSize(int maxSize) {
+      options.setMaxSize(maxSize);
+      return this;
     }
 
-    public int getMaxHeight() {
-        return maxHeight;
+    public Builder setMaxHeight(int maxHeight) {
+      options.setMaxHeight(maxHeight);
+      return this;
     }
 
-    public void setMaxHeight(int maxHeight) {
-        this.maxHeight = maxHeight;
+    public Builder setMaxWidth(int maxWidth) {
+      options.setMaxWidth(maxWidth);
+      return this;
     }
 
-    public int getMaxWidth() {
-        return maxWidth;
+    public Builder setGear(int gear) {
+      return this;
     }
 
-    public void setMaxWidth(int maxWidth) {
-        this.maxWidth = maxWidth;
+    public LubanOptions create() {
+      return options;
     }
-
-    public int getGear() {
-        return gear;
-    }
-
-    public void setGear(int gear) {
-        this.gear = gear;
-    }
-
-    public static class Builder{
-        private LubanOptions options;
-
-        public Builder() {
-            options=new LubanOptions();
-        }
-
-        public Builder setMaxSize(int maxSize) {
-            options.setMaxSize(maxSize);
-            return this;
-        }
-
-        public Builder setMaxHeight(int maxHeight) {
-            options.setMaxHeight(maxHeight);
-            return this;
-        }
-
-        public Builder setMaxWidth(int maxWidth) {
-            options.setMaxWidth(maxWidth);
-            return this;
-        }
-
-        public Builder setGear(int gear) {
-            options.setGear(gear);
-            return this;
-        }
-        public LubanOptions create(){
-            return options;
-        }
-    }
+  }
 }
