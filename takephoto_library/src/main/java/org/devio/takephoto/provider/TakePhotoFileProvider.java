1a19863cd5ce8770ce7d31c721dc0bfa6f3d55a9
suhao <2301230182@qq.com>
2018-04-13 15:28:40 +0800

    将FileProvider改为继承它的TakephotoFileProvider,防止开发者集成后与项目中已存在的FileProvider产生冲突

---
+++ takephoto_library/src/main/java/org/devio/takephoto/provider/TakePhotoFileProvider.java
@@ -0,0 +1,10 @@
+package org.devio.takephoto.provider;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by moos on 2018/4/13.
+ */
+
+public class TakePhotoFileProvider extends FileProvider {
+}
